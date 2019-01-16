60ecd13cdf3579502d52ec4ffb957ec40aa9ac5f
Microvoice <Microvoice@DESKTOP-PJ10M2P>
2019-01-16 14:57:17 +0100

    main

--- MicroKSiw2019/src/model/main.java
+++ MicroKSiw2019/src/model/main.java
@@ -3,14 +3,14 @@ package model;
 public class main {
 
 	public static void main(String[] args) {
-		CatalogoMusicale c;
+/*		CatalogoMusicale c;
 		c=new CatalogoMusicale();
 		Artista a=new Artista("adriano", "celentano");
 		Canzone cc =new Canzone();
 		c.inserisciUnArtista(a);
 		System.out.println(a.generaIdArtista(a));
 		// TODO Auto-generated method stub
-
+*/
 	}
 
 }
