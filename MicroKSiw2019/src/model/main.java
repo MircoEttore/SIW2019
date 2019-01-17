7b0e26143cbc4c881d271f621ac061327c295c89
Ettor <Ettor@DESKTOP-MGM63U6>
2019-01-17 11:12:45 +0100

    boh

--- MicroKSiw2019/src/model/main.java
+++ MicroKSiw2019/src/model/main.java
@@ -3,12 +3,12 @@ package model;
 public class main {
 
 	public static void main(String[] args) {
-		CatalogoMusicale c;
-		c=new CatalogoMusicale();
-		Artista a=new Artista("adriano", "celentano");
-		Canzone cc =new Canzone();
-		c.inserisciUnArtista(a);
-		System.out.println(a.generaIdArtista(a));
+//		CatalogoMusicale c;
+//		c=new CatalogoMusicale();
+//		Artista a=new Artista("adriano", "celentano");
+//		Canzone cc =new Canzone();
+//		c.inserisciUnArtista(a);
+//		System.out.println(a.generaIdArtista(a));
 		// TODO Auto-generated method stub
 
 	}
