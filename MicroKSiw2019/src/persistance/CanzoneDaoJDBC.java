05036d2b4dc593ba2626bd281722cf5f3aaa0fde
Microvoice <Microvoice@DESKTOP-PJ10M2P>
2019-01-18 10:55:19 +0100

    df

--- MicroKSiw2019/src/persistance/CanzoneDaoJDBC.java
+++ MicroKSiw2019/src/persistance/CanzoneDaoJDBC.java
@@ -2,10 +2,8 @@ package persistance;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
 
 
 import model.Canzone;
