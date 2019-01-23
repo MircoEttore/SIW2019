8b10bfeadbd24065d31115a77ffff1e9423a03af
Microvoice <Microvoice@DESKTOP-PJ10M2P>
2019-01-23 11:23:00 +0100

    a

---
+++ MicroKSiw2019/src/persistance/JDBC/CategorieDaoJDBC.java
@@ -0,0 +1,85 @@
+package persistance.JDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import persistance.DataSource;
+import persistence.dao.CategorieDAO;
+
+public class CategorieDaoJDBC implements CategorieDAO  {
+	
+/*
+		private DataSource dataSource;
+
+		public CategorieDAOJDBC(DataSource dataSource) {
+			
+			this.dataSource = dataSource;
+		}
+
+		@Override
+		public Category findByPrimaryKey(String categoryName) {
+			Connection connection = this.dataSource.getConnection();
+			Category category = null;
+			try {
+				PreparedStatement statement;
+				String query = "select * from category where name=?";
+				statement = (PreparedStatement) connection.prepareStatement(query);
+				statement.setString(1, categoryName);
+				ResultSet results = statement.executeQuery();
+				while (results.next()) {
+					category = new Category();
+					category.setId(results.getInt("id"));
+					category.setName(results.getString("name"));
+				}
+
+			} catch (Exception e) {
+				return null;
+				// throw new PersistenceException(e.getMessage());
+			} finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					return null;
+					// throw new PersistenceException(e.getMessage());
+				}
+			}
+			try {
+				if (!connection.isClosed())
+					connection.close();
+			} catch (SQLException e) {
+				// e.printStackTrace();
+			}
+			return category;
+		}
+
+		@Override
+		public List<Category> findAll() {
+			Connection connection = this.dataSource.getConnection();
+			List<Category> categories = new ArrayList<>();
+			try {
+				Category category = null;
+				PreparedStatement statement;
+				String query = "select * from category";
+				statement = connection.prepareStatement(query);
+				ResultSet result = statement.executeQuery();
+				while (result.next()) {
+					category = new Category();
+					category.setId(result.getInt("id"));
+					category.setName(result.getString("name"));
+					categories.add(category);
+				}
+			} catch (SQLException e) {
+				// throw new PersistenceException(e.getMessage());
+			} finally {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					// throw new PersistenceException(e.getMessage());
+				}
+			}
+			return categories;
+		}
+
+	}
+*/
+}
---
+++ MicroKSiw2019/src/persistence/dao/CategorieDAO.java
@@ -0,0 +1,11 @@
+package persistence.dao;
+
+import java.util.List;
+
+import model.CatalogoMusicale;
+
+public interface CategorieDAO {/*
+	public CatalogoMusicale findByPrimaryKey(String name); // Retrieve
+
+	public List<CatalogoMusicale> findAll();*/
+}
