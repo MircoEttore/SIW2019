60c7f46b92f6871ea8312ef6c1de1217cabb11c6
Microvoice <Microvoice@DESKTOP-PJ10M2P>
2019-01-26 12:04:11 +0100

    Tutto apposto

--- NewFoldenUpdate/MicroKSiw2019_2/src/servlet/LoginIN.java
+++ NewFoldenUpdate/MicroKSiw2019_2/src/servlet/LoginIN.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginIN
  */
-@WebServlet("/LoginIN")
 public class LoginIN extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
